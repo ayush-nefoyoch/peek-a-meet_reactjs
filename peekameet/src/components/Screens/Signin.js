ed1ab916ce1486f1fb3c428f0ac92d13dba169dd
nefoyoch <ayush@unthinkable.co>
2021-12-06 12:10:25 +0530

    signin ui

--- peekameet/src/components/Screens/Signin.js
+++ peekameet/src/components/Screens/Signin.js
@@ -1,58 +1,89 @@
 import React from "react";
 import './Signin.css';
-
+import signin from '../../assets/images/signin.png';
 
 export const Signin = () => {
   return (
-    <div>
-      <nav class="navbar navbar-dark bg-dark">
+    <>
+    <div className="firstContainer">
+      <nav class="navbar">
         <a class="navbar-brand" href="https://reactjs.org">
           <img
             src="/docs/4.0/assets/brand/bootstrap-solid.svg"
             width="30"
             height="30"
-            class="d-inline-block align-top"
+            className="d-inline-block align-top logoTxt"
             alt=""
           />
-          Bootstrap
+          PEEKaMEET
         </a>
-
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="/signin">
-              Home <span class="sr-only">(current)</span>
+<div className="navItems">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="/signin">
+              Sign In <span className="sr-only">(current)</span>
+            </a>
+          </li>
+            <li>
+            <a className="nav-link" href="/signin">
+              Sign Up <span className="sr-only">(current)</span>
             </a>
           </li>
         </ul>
+       </div>
       </nav>
       <div className="centerContainer">
       <div className="card mb-3 mt-3 first">
           <div className="row no-gutters">
-            <div className="col-md-6">
-              <img src="" alt="..." />
+            <div className="col-md-6 signinImage">
+              <img src={signin} className="signinImage" alt="..." />
             </div>
             <div className="col-md-6">
               <div className="card-body">
-                <h5 className="card-title">Lifecycle of Components</h5>
-                <p className="card-text pb-3">
-                  Each component in React has a lifecycle which you can monitor
-                  and manipulate during its three main phases.
-                  <br />
-                  <br />
-                  The three main phases are:
-                  <ul className="pb-2">
-                    <li>Mounting</li>
-                    <li>Updating</li>
-                    <li>UnMounting</li>
-                  </ul>
+                <h5 className="card-title">
+                <span className="text-style-1">PEEKaMEET </span>lets you network more effectively to achieve your business and career goals
+                </h5>
+                <div className="card-text pb-3 radiusSection">
+                  <span className="freelancer">Freelancer</span>
+                  <span className="job">Job Seeker</span>
+                  <span className="entrepreneur">Entrepreneur</span>
+                  <span className="mompreneur">Mompreneur</span><br/><br/>
+                  <span className="intern">Internship Seeker</span>
+                  <span className="environment">Environmental Change Maker</span>
                   {/* Mounting, Updating, and Unmounting. */}
-                </p>
-                {/* <p class="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
+                </div>
+                <p className="card-text buildText">Build and manage your network with PEEKaMEET</p>
               </div>
+
+              {/* form-------- */}
+              
+              <form action="/action_page.php">
+  <div class="form-group">
+    <label for="email" className="Email">Email</label>
+    <input type="email" className="form-control" id="email"/>
+  </div>
+  <div class="form-group">
+    <label for="pwd" className="password">Password:</label>
+    <input type="password" className="form-control" id="pwd"/>
+  </div>
+  <button type="submit" className="btn btn-default submit">Sign In</button>
+  <div class="checkbox">
+    <label><input type="checkbox"/> Remember me</label>
+    <span className="forgot">Forgot Password</span>
+  </div>
+  <div class="signup">
+    <p>Don’t have an account? <span className="signupLine">Sign Up</span></p>
+  </div>
+</form>
+
+
+
             </div>
           </div>
         </div>
     </div>
     </div>
+    <h1>sdsdsdd</h1>
+    </>
   );
 };
