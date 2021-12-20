8ae02aa316a4b0f00995b72006b5ea68dd8e4451
nefoyoch <ayush@unthinkable.co>
2021-12-20 15:26:28 +0530

    remove extra spaces

--- peekameet/src/redux/createStore.js
+++ peekameet/src/redux/createStore.js
@@ -2,6 +2,5 @@ import {applyMiddleware, createStore} from 'redux';
 import rootReducers from './reducer';
 import thunk from 'redux-thunk';
 
-
 const store = createStore(rootReducers, applyMiddleware(thunk));
 export default store;
